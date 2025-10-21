4763e398137f84035f63492493152372d32528e5
erfan-ne <erfan.neshati@gmail.com>
2025-10-21 23:38:33 +0330

    feat(frontend): update TopTechnicians component

--- Handi-Frontend/src/Pages/Customer/components/TopTechnicians.jsx
+++ Handi-Frontend/src/Pages/Customer/components/TopTechnicians.jsx
@@ -2,6 +2,7 @@ import { Box, Paper, Typography } from "@mui/material";
 import React from "react";
 import Avatar from "@mui/material/Avatar";
 import StarIcon from "@mui/icons-material/Star";
+import Divider from "@mui/material/Divider";
 
 export default function TopTechnicians({ avatar, fullName, expertise, rate }) {
   return (
@@ -27,21 +28,35 @@ export default function TopTechnicians({ avatar, fullName, expertise, rate }) {
         </Typography>
       </Box>
 
-      <Box sx={{display:"flex", alignItems:"center", justifyContent:"space-between"}}>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
         {/* Left Side (avatar + fullName + expertise) */}
         <Box mt={4} sx={{ display: "flex" }}>
           <Avatar alt={fullName} src={avatar} sx={{ width: 45, height: 45 }} />
-          <Box sx={{ display: "flex", flexDirection: "column", ml:2, alignItems:"flex-start" }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              ml: 2,
+              alignItems: "flex-start",
+            }}
+          >
             <Typography>{fullName}عرفان نشاطی</Typography>
             <Typography>تخصص:{expertise}</Typography>
           </Box>
         </Box>
 
         {/* Right Side (icon + rate) */}
-        <Box sx={{display:"flex"}}>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Typography mt={1} mr={1} color="text.secondary">
+            {rate}4.9
+          </Typography>
           <StarIcon sx={{ fontSize: 30, color: "secondary.main" }}></StarIcon>
         </Box>
       </Box>
+      <Divider
+        variant="middle"
+        sx={{ mt: 2, width: "70%", mx: "auto", borderColor: "secondary.dark" }}
+      />
     </Paper>
   );
 }
