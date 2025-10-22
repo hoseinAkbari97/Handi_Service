4f0bc992ed6979334c8101f9f11067e7b55bd81c
erfan-ne <erfan.neshati@gmail.com>
2025-10-22 13:50:44 +0330

    feat(frontend): create SummaryCard component for techniciansDashboard

--- Handi-Frontend/src/Pages/Technician/Technicians.jsx
+++ Handi-Frontend/src/Pages/Technician/Technicians.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box } from "@mui/material";
 import Header from "./Components/Header";
+import SummaryCard from "./Components/SummaryCard";
 
 export default function CustomerDashboard() {
   return (
@@ -8,6 +9,18 @@ export default function CustomerDashboard() {
 
       {/* Heaedr */}
       <Header />
+
+            {/* Summary Section */}
+            <Box mt={3} display="grid" gap={1}>
+        <SummaryCard iconType="workDone" label="کارهای انجام شده" value="۱۲" />
+        <SummaryCard iconType="Income" label="درآمد این ماه" value="۴,۵۰۰,۰۰۰ تومان" />
+        <SummaryCard
+          iconType="rate"
+          label="میانگین امتیاز"
+          value="۴.۹"
+        />
+        <SummaryCard iconType="clock" label="میانگین زمان پاسخگویی" value="۲۵ دقیقه" />
+      </Box>
 
     </Box>
   );
