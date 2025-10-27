9a26a06d2ed542451c2e5028e266038e02d4ce93
erfan-ne <erfan.neshati@gmail.com>
2025-10-27 16:45:09 +0330

    feat(frontend): create AgentDashboard

--- Handi-Frontend/src/App.jsx
+++ Handi-Frontend/src/App.jsx
@@ -3,8 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import theme from "./Theme/Theme";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login";
-import Customer from "./Pages/Customer/CustomerDashboard";
-import Technicians from "./Pages/Technician/Technicians";
+import CustomerDashboard from "./Pages/Customer/CustomerDashboard";
+import TechnicianDashboard from "./Pages/Technician/TechnicianDashboard";
+import AgentDashboard from "./Pages/Agent/AgentDashboard"
 
 function App() {
   return (
@@ -14,8 +15,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/customer" element={<Customer />} />
-          <Route path="/technicians" element={<Technicians />} />
+          <Route path="/customer" element={<CustomerDashboard />} />
+          <Route path="/technician" element={<TechnicianDashboard />} />
+          <Route path="/agent" element={<AgentDashboard />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
