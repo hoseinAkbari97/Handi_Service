251395be1e8839b392d3ee76be317558ac298e15
erfan-ne <erfan.neshati@gmail.com>
2025-10-29 17:59:15 +0330

    fix(frontend): update folder structure

---
+++ Handi-Frontend/src/Pages/Agent/Components/TechnicianCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+export default function TechnicianCard() {
+  return (
+    <Box sx={{backgroundColor:"primary.main", width:120, height:120}}>
+      
+    </Box>
+  )
+}
