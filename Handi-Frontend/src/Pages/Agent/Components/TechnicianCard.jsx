3da3a990a0d82d0e886fc49f70280e330797e579
erfan-ne <erfan.neshati@gmail.com>
2025-10-30 13:25:15 +0330

    feat(frontend): TechnicianCard component completed

--- Handi-Frontend/src/Pages/Agent/Components/TechnicianCard.jsx
+++ Handi-Frontend/src/Pages/Agent/Components/TechnicianCard.jsx
@@ -1,28 +1,136 @@
-import React from 'react'
-import { Box, Avatar, Typography } from '@mui/material'
+import React from "react";
+import { Box, Avatar, Typography, Button } from "@mui/material";
+import { Star } from "@mui/icons-material";
+import PhoneIcon from "@mui/icons-material/Phone";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
-
-export default function TechnicianCard({technician}) {
+export default function TechnicianCard({ technician }) {
   return (
-    <Box sx={{backgroundColor:"primary.main", width:220, height:320, borderRadius:3}}>
-
+    <Box
+      sx={{
+        display:"flex",
+        flexDirection:"column",
+        backgroundColor: "primary.main",
+        width: 220,
+        height: 350,
+        borderRadius: 3,
+        pb:0
+      }}
+    >
       <Avatar
-            src={technician?.avatar || ""}
-            alt={technician?.name || ""}
-            sx={{ width: 74, height: 75, mt: -4, mx: "auto" }}
-          />
+        src={technician?.avatar || ""}
+        alt={technician?.name || ""}
+        sx={{ width: 74, height: 75, mt: -4, mx: "auto" }}
+      />
 
-      <Typography
-        sx={{
-          fontSize: 18,
-          fontWeight: "bold",
-          mt: 2,
-          textAlign: "center",
-          color:"text.secondary"
-        }}
-      >
-        {technician.fullName}
-      </Typography>
+      <Box>
+        <Typography
+          sx={{
+            fontSize: 18,
+            fontWeight: "bold",
+            mt: 2,
+            textAlign: "center",
+            color: "text.secondary",
+          }}
+        >
+          {technician.fullName}
+        </Typography>
+
+        <Typography
+          sx={{
+            fontSize: 12,
+            mt: 2,
+            textAlign: "center",
+          }}
+        >
+          تخصص: {technician.expertise}
+        </Typography>
+
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 0.5,
+          }}
+        >
+          <Star color="secondary" />
+          <Typography
+            sx={{
+              fontSize: 16,
+              mt: 2,
+              textAlign: "center",
+              color: "text.secondary",
+              mb: 1,
+            }}
+          >
+            {technician.rate}
+          </Typography>
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 0.5,
+          }}
+        >
+          <PhoneIcon />
+          <Typography
+            sx={{
+              fontSize: 16,
+              mt: 2,
+              textAlign: "center",
+
+              mb: 1,
+            }}
+          >
+            {technician.phone}
+          </Typography>
+        </Box>
+
+        <Box
+          sx={{
+            width: "fit-content",
+            px: 3,
+            mt: 1,
+            height: 40,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor:
+              technician.status === "available"
+                ? "success.main"
+                : technician.status === "busy"
+                ? "warning.light"
+                : "error.main",
+            borderRadius: 10,
+            mx: "auto",
+          }}
+        >
+          <Typography>
+            {technician.status === "available"
+              ? "آماده به کار"
+              : technician.status === "busy"
+              ? "درحال انجام کار"
+              : "آفلاین"}
+          </Typography>
+        </Box>
+
+        <Box sx={{display:"flex", justifyContent:"center", alignSelf:"", mt:1, width:"100%"}}>
+          <ButtonGroup
+            size="large"
+            variant="contained"
+            aria-label="Basic button group"
+            color="secondary"
+            sx={{ mt: 1, justifySelf: "center" }}
+          >
+            <Button>جزئیات</Button>
+            <Button>ویرایش</Button>
+          </ButtonGroup>
+        </Box>
+      </Box>
     </Box>
-  )
+  );
 }
