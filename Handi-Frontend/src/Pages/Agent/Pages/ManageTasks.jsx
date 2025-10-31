df1a8a4094fc2c9b694c1f81890d5207f493cdba
erfan-ne <erfan.neshati@gmail.com>
2025-10-31 13:43:56 +0330

    feat(frontend): create TaskCard component

--- Handi-Frontend/src/Pages/Agent/Pages/ManageTasks.jsx
+++ Handi-Frontend/src/Pages/Agent/Pages/ManageTasks.jsx
@@ -1,7 +1,13 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import TaskCard from '../Components/TaskCard'
+import { TasksList } from '../../../Datas'
+
 
 export default function ManageTasks() {
   return (
-    <div>ManageTasks</div>
+    <Box>
+      <TaskCard task={TasksList[0]}/>
+    </Box>
   )
 }
