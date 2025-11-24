ee69cca1e9b204c97d3e93d7f304cdac4307e806
erfan-ne <erfan.neshati@gmail.com>
2025-11-24 13:22:10 +0330

    feat(frontend): update ActiveRequestCard in CustomerDashboard

--- Handi-Frontend/src/Pages/Customer/components/ActiveRequestCard.jsx
+++ Handi-Frontend/src/Pages/Customer/components/ActiveRequestCard.jsx
@@ -1,13 +1,17 @@
 import { Box, Typography, Button } from "@mui/material";
 import AcUnitIcon from "@mui/icons-material/AcUnit";
 import EngineeringIcon from "@mui/icons-material/Engineering";
+import { UserContext } from "../../../Contexts/UserContext";
+import { useContext } from "react";
 
 export default function ActiveRequestCard() {
+  const { user } = useContext(UserContext);
+
   return (
     <Box
       sx={{
-        display:"flex",
-        flexDirection:"column",
+        display: "flex",
+        flexDirection: "column",
         backgroundColor: "primary.main",
         border: "solid 2px",
         borderColor: "secondary.main",
@@ -18,60 +22,73 @@ export default function ActiveRequestCard() {
         boxShadow: 3,
         m: "0 auto",
         width: "100%",
-        height:{lg:"100%", xl:"350px"},
-        justifyContent:"center"
+        height: { lg: "100%", xl: "350px" },
+        justifyContent: "center",
       }}
     >
-      <Box>
+      <Typography
+        variant="h5"
+        color="secondary"
+        sx={{ mb: 2, fontWeight: "bold" }}
+      >
+        درخواست فعال شما
+      </Typography>
+
+      {user.active_request ? (
+        <>
+          <Box>
+            <Typography
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <AcUnitIcon fontSize="small" sx={{ mr: 0.5 }} /> تعمیر یخچال فریزر
+              سامسونگ
+            </Typography>
+            <Typography
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                mt: 1,
+              }}
+            >
+              <EngineeringIcon sx={{ mr: 0.5 }} /> تعمیرکار: علی رضایی
+            </Typography>
+            <Typography
+              variant="h5"
+              color="secondary"
+              sx={{ mt: 2, fontWeight: "bold" }}
+            >
+              هزینه: ۲۵۰,۰۰۰ تومان
+            </Typography>
+          </Box>
+          <Box>
+            <Button
+              variant="contained"
+              color="secondary"
+              sx={{
+                mt: 2,
+                borderRadius: 1.5,
+                fontSize: 17,
+                width: "100%",
+              }}
+            >
+              پرداخت هزینه
+            </Button>
+          </Box>
+        </>
+      ) : (
         <Typography
-          variant="h5"
-          color="secondary"
+          variant="body2"
+          color="text.dark"
           sx={{ mb: 2, fontWeight: "bold" }}
         >
-          درخواست فعال شما
+          هیچ درخواست فعالی وجود ندارد
         </Typography>
-        <Typography
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <AcUnitIcon fontSize="small" sx={{ mr: 0.5 }} /> تعمیر یخچال فریزر
-          سامسونگ
-        </Typography>
-        <Typography
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            mt: 1,
-          }}
-        >
-          <EngineeringIcon sx={{ mr: 0.5 }} /> تعمیرکار: علی رضایی
-        </Typography>
-        <Typography
-          variant="h5"
-          color="secondary"
-          sx={{ mt: 2, fontWeight: "bold" }}
-        >
-          هزینه: ۲۵۰,۰۰۰ تومان
-        </Typography>
-      </Box>
-      <Box>
-      <Button
-        variant="contained"
-        color="secondary"
-        sx={{
-          mt: 2,
-          borderRadius: 1.5,
-          fontSize: 17,
-          width:"100%",
-        }}
-      >
-        پرداخت هزینه
-      </Button>
-      </Box>
+      )}
     </Box>
   );
 }
