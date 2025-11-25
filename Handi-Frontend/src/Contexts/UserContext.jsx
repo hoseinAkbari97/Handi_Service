53170706f2e74c812284ff364037a99b5151acad
erfan-ne <erfan.neshati@gmail.com>
2025-11-25 14:58:00 +0330

    fix(frontend): optimization CustomerDashboard & TechnicianDashbord

--- Handi-Frontend/src/Contexts/UserContext.jsx
+++ Handi-Frontend/src/Contexts/UserContext.jsx
@@ -1,4 +1,11 @@
-import { createContext, useState, useEffect } from "react";
+import {
+  createContext,
+  useState,
+  useEffect,
+  useCallback,
+useMemo
+} from "react";
+import { Navigate } from "react-router-dom";
 
 export const UserContext = createContext();
 
@@ -12,107 +19,170 @@ export default function UserProvider({ children }) {
     }
   }, []);
 
-  const saveUser = (userData) => {
+  const saveUser = useCallback((userData) => {
     setUser(userData);
     localStorage.setItem("user", JSON.stringify(userData));
-  };
+  }, []);
 
-  const requestOTP = async (phone) => {
-        const url = "http://127.0.0.1:8000/api/auth/request-otp/";
-        try {
-            const response = await fetch(url, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ phone }),
-            });
+  const requestOTP = useCallback(async (phone) => {
+    const url = "http://127.0.0.1:8000/api/auth/request-otp/";
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ phone }),
+      });
 
-            if (!response.ok) {
-                throw new Error("ارسال پیامک با خطا مواجه شد.");
-            }
-        } catch (error) {
-            console.error("Request OTP failed:", error);
-            throw error;
+      if (!response.ok) {
+        throw new Error("ارسال پیامک با خطا مواجه شد.");
+      }
+    } catch (error) {
+      console.error("Request OTP failed:", error);
+      throw error;
+    }
+  }, []);
+
+  const verifyAndLogin = useCallback(
+    async (phone, otp, role) => {
+      const verifyUrl = "http://127.0.0.1:8000/api/auth/verify-otp/";
+      let tokenData;
+      try {
+        const verifyResponse = await fetch(verifyUrl, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ phone, otp }),
+        });
+
+        if (!verifyResponse.ok) {
+          alert("کد وارد شده نامعتبر است.");
+          throw new Error("OTP not valid");
         }
-    };
+        tokenData = await verifyResponse.json();
+      } catch (error) {
+        console.error("OTP verification failed:", error);
+        throw error;
+      }
 
-    const verifyAndLogin = async (phone, otp, role) => {
-        const verifyUrl = "http://127.0.0.1:8000/api/auth/verify-otp/";
-        let tokenData;
-        try {
-            const verifyResponse = await fetch(verifyUrl, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ phone, otp }),
-            });
+      const accessToken = tokenData.access;
+      let dashboardUrl;
+      let errorMessage;
+      let redirectPath;
 
-            if (!verifyResponse.ok) {
-                alert("کد وارد شده نامعتبر است.")
-                throw new Error("OTP not valid");
-            }
-            tokenData = await verifyResponse.json();
-        } catch (error) {
-            console.error("OTP verification failed:", error);
-            throw error;
+      switch (role) {
+        case "customer":
+          dashboardUrl =
+            "http://127.0.0.1:8000/api/service/dashboard/customer/";
+          errorMessage = "مشتری با این شماره وجود ندارد";
+          redirectPath = "/customer";
+          break;
+        case "technician":
+          dashboardUrl =
+            "http://127.0.0.1:8000/api/service/dashboard/technician/";
+          errorMessage = "تعمیرکار با این شماره وجود ندارد";
+          redirectPath = "/technician";
+          break;
+        case "agent":
+          dashboardUrl = "http://127.0.0.1:8000/api/service/dashboard/agent/";
+          errorMessage = "نماینده‌ای با این شماره وجود ندارد";
+          redirectPath = "/agent";
+          break;
+        default:
+          throw new Error("نقش کاربری نامعتبر است.");
+      }
+
+      try {
+        const dashboardResponse = await fetch(dashboardUrl, {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        });
+
+        if (!dashboardResponse.ok) {
+          if (dashboardResponse.status === 403) {
+            alert(errorMessage);
+          }
+          throw new Error("خطا در دریافت اطلاعات داشبورد.");
         }
 
-        const accessToken = tokenData.access;
-        let dashboardUrl;
-        let errorMessage;
-        let redirectPath;
+        const userData = await dashboardResponse.json();
 
-        switch (role) {
-            case "customer":
-                dashboardUrl = "http://127.0.0.1:8000/api/service/dashboard/customer/";
-                errorMessage = "مشتری با این شماره وجود ندارد";
-                redirectPath = "/customer";
-                break;
-            case "technician":
-                dashboardUrl = "http://127.0.0.1:8000/api/service/dashboard/technician/";
-                errorMessage = "تعمیرکار با این شماره وجود ندارد";
-                redirectPath = "/technician";
-                break;
-            case "agent":
-                dashboardUrl = "http://127.0.0.1:8000/api/service/dashboard/agent/";
-                errorMessage = "نماینده‌ای با این شماره وجود ندارد";
-                redirectPath = "/agent";
-                break;
-            default:
-                throw new Error("نقش کاربری نامعتبر است.");
-        }
+        saveUser({ ...userData, access: accessToken });
+        localStorage.setItem("role", userData.profile.user_type);
+        localStorage.setItem("isValid", "true");
 
-        try {
-            const dashboardResponse = await fetch(dashboardUrl, {
-                headers: { Authorization: `Bearer ${accessToken}` },
-            });
+        return redirectPath;
+      } catch (error) {
+        console.error("Dashboard data fetch failed:", error);
+        throw error;
+      }
+    },
+    [saveUser]
+  );
 
-            if (!dashboardResponse.ok) {
-                if (dashboardResponse.status === 403) {
-                    alert(errorMessage); 
-                }
-                throw new Error("خطا در دریافت اطلاعات داشبورد.");
-            }
+  const reFetchUser = useCallback(async () => {
+    if (!user || !user.access || !user.profile || !user.profile.user_type) {
+      console.error("User or Access Token not available for refetch.");
+      return;
+    }
 
-            const userData = await dashboardResponse.json();
-       
-            saveUser({ ...userData, access: accessToken }); 
-            localStorage.setItem("role", userData.profile.user_type);
-            localStorage.setItem("isValid", "true");
+    const role = user.profile.user_type;
+    const accessToken = user.access;
 
-            return redirectPath;
-        } catch (error) {
-            console.error("Dashboard data fetch failed:", error);
-            throw error;
-        }
-    };
+    let dashboardUrl;
 
-  const logout = () => {
-    localStorage.removeItem("user");
+    switch (role) {
+      case "customer":
+        dashboardUrl = "http://127.0.0.1:8000/api/service/dashboard/customer/";
+        break;
+      case "technician":
+        dashboardUrl =
+          "http://127.0.0.1:8000/api/service/dashboard/technician/";
+        break;
+      case "agent":
+        dashboardUrl = "http://127.0.0.1:8000/api/service/dashboard/agent/";
+        break;
+      default:
+        return;
+    }
+
+    try {
+      const response = await fetch(dashboardUrl, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      });
+
+      if (!response.ok || !accessToken) {
+        console.error("Failed to refetch user data. Status:", response.status);
+        logout();
+        return;
+      }
+
+      const newUserData = await response.json();
+
+      saveUser({ ...newUserData, access: accessToken });
+    } catch (error) {
+      console.error("Error during user refetch:", error);
+    }
+  }, [user, saveUser]);
+
+  const logout = useCallback(() => {
     setUser(null);
-  };
+    localStorage.removeItem("isValid");
+    localStorage.removeItem("role");
+    localStorage.removeItem("user");
+    Navigate((to = "/"));
+  }, [Navigate]);
+
+  const contextValue = useMemo(
+    () => ({
+      user,
+      saveUser,
+      requestOTP,
+      verifyAndLogin,
+      reFetchUser,
+      logout,
+    }),
+    [user, saveUser, requestOTP, verifyAndLogin, reFetchUser, logout]
+  );
 
   return (
-    <UserContext.Provider value={{ user, saveUser, requestOTP, verifyAndLogin, logout }}>
-      {children}
-    </UserContext.Provider>
+    <UserContext.Provider value={contextValue}>{children}</UserContext.Provider>
   );
 }
