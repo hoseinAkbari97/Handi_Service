5fb1b8037937bb4f83baf97581f2063e659738a0
erfan-ne <erfan.neshati@gmail.com>
2025-11-27 16:18:54 +0330

    fix(frontend): fixed logout function in UserContext

--- Handi-Frontend/src/App.jsx
+++ Handi-Frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import { BrowserRouter, useRoutes } from "react-router-dom";
+import { BrowserRouter, useRoutes, useNavigate } from "react-router-dom";
 import { CacheProvider } from "@emotion/react";
 import theme from "./Theme/Theme";
 import routes from "./routes/routes";
 import rtlCache from "./Theme/RTLCache";
+import UserProvider from "./Contexts/UserContext";
 
 function AppRoutes() {
   const routing = useRoutes(routes);
   return routing;
+}
+
+function UserProviderWrapper({ children }) {
+  const navigate = useNavigate();
+
+  return <UserProvider navigate={navigate}>{children}</UserProvider>;
 }
 
 function App() {
@@ -16,7 +23,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <BrowserRouter>
-          <AppRoutes />
+          <UserProviderWrapper>
+            <AppRoutes />
+          </UserProviderWrapper>
         </BrowserRouter>
       </ThemeProvider>
     </CacheProvider>
