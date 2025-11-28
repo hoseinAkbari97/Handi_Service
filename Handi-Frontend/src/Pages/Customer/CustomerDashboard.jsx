2834c14e087c994718082ed02405eab751ba14c2
erfan-ne <erfan.neshati@gmail.com>
2025-11-28 11:20:02 +0330

    create Dashboard page in CustomerDashboard

--- Handi-Frontend/src/Pages/Customer/CustomerDashboard.jsx
+++ Handi-Frontend/src/Pages/Customer/CustomerDashboard.jsx
@@ -7,6 +7,7 @@ import Header from "./Components/Header";
 import Sidebar from "../../Layout/Sidebar";
 import { UserContext } from "../../Contexts/UserContext";
 import { toPersianNumber } from "../../Utils/NumberUtils";
+import { Outlet } from "react-router-dom";
 
 export default function CustomerDashboard() {
   const { user, reFetchUser } = useContext(UserContext);
@@ -67,64 +68,7 @@ export default function CustomerDashboard() {
         {/* Heaedr */}
         <Header />
 
-        <Box
-          sx={{
-            flexGrow: "1",
-            display: { lg: "grid" },
-            gridTemplateColumns: { lg: "1.5fr 1fr", xl: "1fr 1fr 1fr" },
-            gridTemplateRows: { lg: "auto auto", xl: "auto" },
-            gap: 1,
-            gridTemplateAreas: {
-              lg: `"active summary"
-                   "topTech summary"`,
-              xl: `"active topTech summary"`,
-            },
-          }}
-        >
-          {/* Active Request */}
-          <Box sx={{ gridArea: { lg: "active", xl: "active" } }}>
-            <ActiveRequestCard />
-          </Box>
-
-          {/* Summary Section */}
-          <Box
-            mt={{ xs: 3, lg: 0 }}
-            display="grid"
-            gap={1}
-            gridTemplateColumns={{
-              xs: "1fr",
-              md: "1fr 1fr",
-              lg: "1fr",
-            }}
-            sx={{ gridArea: { lg: "summary", xl: "summary" } }}
-          >
-            <SummaryCard
-              iconType="requests"
-              label="کل درخواست‌ها"
-              value={user.total_requests}
-            />
-            <SummaryCard
-              iconType="done"
-              label="خدمات انجام‌شده"
-              value={user.completed_requests}
-            />
-            <SummaryCard
-              iconType="wallet"
-              label="موجودی کیف پول"
-              value={`${toPersianNumber(user.wallet_balance)} تومان`}
-            />
-          </Box>
-
-          {/* Top Technicians */}
-          <Box
-            sx={{
-              gridArea: { lg: "topTech", xl: "topTech" },
-              mt: { xs: 2, lg: 0 },
-            }}
-          >
-            <TopTechnicians technicians={user.top_technicians} />
-          </Box>
-        </Box>
+        <Outlet />
       </Box>
     </Box>
   );
--- Handi-Frontend/src/Pages/Customer/Pages/Dashboard.jsx
+++ Handi-Frontend/src/Pages/Customer/Pages/Dashboard.jsx
@@ -1,7 +1,74 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Box } from "@mui/material";
+import ActiveRequestCard from "../Components/ActiveRequestCard";
+import SummaryCard from "../Components/SummaryCard";
+import TopTechnicians from "../Components/TopTechnicians";
+import { UserContext } from "../../../Contexts/UserContext";
+import { toPersianNumber } from "../../../Utils/NumberUtils";
 
 export default function CustomerDashboardPanel() {
+  const { user } = useContext(UserContext);
+
   return (
-    <div>CustomerDashboardPanel</div>
-  )
+    <>
+      <Box
+        sx={{
+          flexGrow: "1",
+          display: { lg: "grid" },
+          gridTemplateColumns: { lg: "1.5fr 1fr", xl: "1fr 1fr 1fr" },
+          gridTemplateRows: { lg: "auto auto", xl: "auto" },
+          gap: 1,
+          gridTemplateAreas: {
+            lg: `"active summary"
+                   "topTech summary"`,
+            xl: `"active topTech summary"`,
+          },
+        }}
+      >
+        {/* Active Request */}
+        <Box sx={{ gridArea: { lg: "active", xl: "active" } }}>
+          <ActiveRequestCard />
+        </Box>
+
+        {/* Summary Section */}
+        <Box
+          mt={{ xs: 3, lg: 0 }}
+          display="grid"
+          gap={1}
+          gridTemplateColumns={{
+            xs: "1fr",
+            md: "1fr 1fr",
+            lg: "1fr",
+          }}
+          sx={{ gridArea: { lg: "summary", xl: "summary" } }}
+        >
+          <SummaryCard
+            iconType="requests"
+            label="کل درخواست‌ها"
+            value={user.total_requests}
+          />
+          <SummaryCard
+            iconType="done"
+            label="خدمات انجام‌شده"
+            value={user.completed_requests}
+          />
+          <SummaryCard
+            iconType="wallet"
+            label="موجودی کیف پول"
+            value={`${toPersianNumber(user.wallet_balance)} تومان`}
+          />
+        </Box>
+
+        {/* Top Technicians */}
+        <Box
+          sx={{
+            gridArea: { lg: "topTech", xl: "topTech" },
+            mt: { xs: 2, lg: 0 },
+          }}
+        >
+          <TopTechnicians technicians={user.top_technicians} />
+        </Box>
+      </Box>
+    </>
+  );
 }
