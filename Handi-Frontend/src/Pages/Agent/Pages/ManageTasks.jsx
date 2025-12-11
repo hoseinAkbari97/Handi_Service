283fd32991910c1278746872366869a4c75fb8d7
erfan-ne <erfan.neshati@gmail.com>
2025-12-11 17:39:22 +0330

    feat(frontend): implementing ManageTasks logic

--- Handi-Frontend/src/Pages/Agent/Pages/ManageTasks.jsx
+++ Handi-Frontend/src/Pages/Agent/Pages/ManageTasks.jsx
@@ -1,21 +1,31 @@
 import React, { useContext, useEffect, useState } from "react";
 import TaskCard from "../Components/TaskCard";
-import NewRequestCard from "../Components/NewRequestCard"
+import NewRequestCard from "../Components/NewRequestCard";
 import { Box } from "@mui/material";
 import { UserContext } from "../../../Contexts/UserContext";
 
 export default function ManageTasks() {
   const { user } = useContext(UserContext);
-  const [Tasks, setTasks] = useState([]);
+  const [allTasks, setAllTasks] = useState([]);
+  const [checkedTasks, setCheckedTasks] = useState([]);
+  const [pendingTasks, setPendingTasks] = useState([]);
 
-    useEffect(() => {
-      fetch("http://127.0.0.1:8000/api/service/dashboard/representative/tasks/", {
-        headers: { Authorization: `Bearer ${user.access}` },
-      })
-        .then((Response) => Response.json())
-        .then((data) => setTasks(data))
-        .catch((error) => console.error("Error fetching data:", error))
-    }, []);
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/service/dashboard/representative/tasks/", {
+      headers: { Authorization: `Bearer ${user.access}` },
+    })
+      .then((Response) => Response.json())
+      .then((data) => setAllTasks(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  useEffect(() => {
+    const pending = allTasks.filter((task)=> task.status === "pending")
+    const checked = allTasks.filter((task)=> task.status !== "pending")
+
+    setPendingTasks(pending);
+    setCheckedTasks(checked)
+  }, [allTasks])
 
   return (
     <Box
@@ -28,13 +38,9 @@ export default function ManageTasks() {
         gridTemplateColumns: "repeat(auto-fit, minmax(280px, 1fr))",
       }}
     >
-      <NewRequestCard />
+      {pendingTasks.map(task => <NewRequestCard task={task}/> )}
+      {checkedTasks.map(task => <TaskCard task={task}/> )}
 
-      {Tasks.map((task, index) => (
-        <Box key={index}>
-          <TaskCard task={task} />
-        </Box>
-      ))}
     </Box>
   );
 }
