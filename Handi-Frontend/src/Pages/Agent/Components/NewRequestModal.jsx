42af36e197a54cbb8451004a79cac76bd7132024
erfan-ne <erfan.neshati@gmail.com>
2025-12-11 22:44:05 +0330

    feat(frontend): create NewReqModal

---
+++ Handi-Frontend/src/Pages/Agent/Components/NewRequestModal.jsx
@@ -0,0 +1,34 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+export default function NewRequestModal({ task, openModal, closeModal }) {
+  return (
+    <div>
+      <Modal open={openModal} onClose={closeModal}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "secondary.main",
+            // width: 280,
+            // height: 380,
+            borderRadius: 3,
+            p: 2,
+          }}
+        >
+          <Typography color="black">Text in a modal</Typography>
+          <Typography color="black">
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
