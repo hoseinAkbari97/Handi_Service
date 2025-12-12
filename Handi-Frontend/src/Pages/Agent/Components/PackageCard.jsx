7741b23f15fa6d54ff10966d8f0fdf2495c65a43
erfan-ne <erfan.neshati@gmail.com>
2025-12-12 12:38:54 +0330

    feat(frontend): create PackageCard.jsx & update NewReqModal

---
+++ Handi-Frontend/src/Pages/Agent/Components/PackageCard.jsx
@@ -0,0 +1,53 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { useState } from "react";
+
+export default function PackageCard({ title, technicians }) {
+  const [technician, setTechnician] = useState("");
+
+  const handleChange = (event) => {
+    setTechnician(event.target.value);
+  };
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        backgroundColor: "primary.main",
+        width: "100%",
+        height: "33%",
+        borderRadius: 3,
+        justifyContent: "space-around",
+        alignItems: "center",
+        px: 4,
+      }}
+    >
+      <Typography
+        sx={{
+          color: "secondary.main",
+          fontWeight: "bold",
+          width: "50%",
+        }}
+      >
+        {title}
+      </Typography>
+      <Box width="50%">
+        <FormControl fullWidth>
+          <InputLabel>تخصیص به تکنسین</InputLabel>
+          <Select
+            value={technician}
+            label="تخصیص به تکنسین"
+            onChange={handleChange}
+          >
+            {technicians.map((technician) => (
+              <MenuItem key={technician.id} value={technician.first_name}>
+                {technician.first_name} {technician.last_name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+    </Box>
+  );
+}
