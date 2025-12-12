fb608370612f63c6c3438c992e9923d8f1e1737b
erfan-ne <erfan.neshati@gmail.com>
2025-12-12 12:38:32 +0330

    feat(frontend): create PackageCard.jsx & update NewReqModal

--- Handi-Frontend/src/Pages/Agent/Components/NewRequestModal.jsx
+++ Handi-Frontend/src/Pages/Agent/Components/NewRequestModal.jsx
@@ -1,23 +1,13 @@
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import {
-  Button,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-} from "@mui/material";
-import { useContext, useState } from "react";
+
+import { useContext } from "react";
 import { UserContext } from "../../../Contexts/UserContext";
+import PackageCard from "./PackageCard";
 
 export default function NewRequestModal({ openModal, closeModal }) {
   const { user } = useContext(UserContext);
-  const [technician, setTechnician] = useState("");
 
-  const handleChange = (event) => {
-    setTechnician(event.target.value);
-  };
 
   return (
     <>
@@ -40,45 +30,10 @@ export default function NewRequestModal({ openModal, closeModal }) {
             p: 2,
           }}
         >
-          <Box
-            sx={{
-              display: "flex",
-              backgroundColor: "primary.main",
-              width: "100%",
-              height: "33%",
-              borderRadius: 3,
-              justifyContent: "space-around",
-              alignItems: "center",
-              px: 4,
-            }}
-          >
-            <Typography
-              sx={{
-                color: "secondary.main",
-                fontWeight: "bold",
-                width: "50%",
-              }}
-            >
-              پکیج عادی
-            </Typography>
-            <Box width="50%">
-              <FormControl fullWidth>
-                <InputLabel>تخصیص به تکنسین</InputLabel>
-                <Select
-                  value={technician}
-                  label="تخصیص به تکنسین"
-                  onChange={handleChange}
-                >
-                  {user.technicians.map((technician) => (
-                    <MenuItem key={technician.id} value={technician.first_name}>
-                      {technician.first_name} {technician.last_name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Box>
-          </Box>
 
+          <PackageCard title={"پکیج عادی"} technicians={user.technicians} />
+          <PackageCard title={"پکیج نقره‌ای"} technicians={user.technicians} />
+          <PackageCard title={"پکیج طلایی"} technicians={user.technicians} />
         </Box>
       </Modal>
     </>
