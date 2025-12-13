f7ae629e0b3fa0d75bc8bb97e69bbbfb05a38c0b
erfan-ne <erfan.neshati@gmail.com>
2025-12-13 14:07:51 +0330

    feat(frontend): create pages of TechnicianDashboard

---
+++ Handi-Frontend/src/Pages/Technician/Pages/Messages.jsx
@@ -0,0 +1,7 @@
+import { Typography } from '@mui/material'
+
+export default function Messages() {
+  return (
+    <Typography textAlign={'center'} variant='h4' color='text.contrastText'>Messages Page</Typography>
+  )
+}
---
+++ Handi-Frontend/src/Pages/Technician/Pages/Settings.jsx
@@ -0,0 +1,7 @@
+import { Typography } from '@mui/material'
+
+export default function Setting() {
+  return (
+    <Typography textAlign={'center'} variant='h4' color='text.contrastText'>Setting</Typography>
+  )
+}
