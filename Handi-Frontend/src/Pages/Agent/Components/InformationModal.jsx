2c32ca51c2d04fe190a093ae64b32543aa7468e5
erfan-ne <erfan.neshati@gmail.com>
2026-06-19 14:44:59 +0330

    create InformationModal base

--- Handi-Frontend/src/Pages/Agent/Components/InformationModal.jsx
+++ Handi-Frontend/src/Pages/Agent/Components/InformationModal.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import { useCallback, useContext, useState } from "react";
+import { UserContext } from "../../../Contexts/UserContext";
+import PackageCard from "./PackageCard";
+import { Button, Typography } from "@mui/material";
+import { API_CONFIG } from "../../../config/api";
 
-function InformationModal() {
+export default function InformationModal({
+  openModal,
+  closeModal,
+  task,
+  refreshTasks,
+}) {
+  const { user } = useContext(UserContext);
+
   return (
-    <div>InformationModal</div>
-  )
-}
+    <>
+      <Modal open={openModal} onClose={closeModal}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            display: "flex",
+            gap: 1,
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "background.default",
+            width: "70%",
+            maxWidth: "60rem",
+            height: "70%",
+            borderRadius: 3,
+            p: 2,
+          }}
+        >
+          {/* {console.log(task)} */}
+          <Typography
+            variant="h5"
+            color="text.contrastText"
+            fontWeight={"bold"}
+            mb={1}
+          >
+            اطلاعات کامل درخواست
+          </Typography>
 
-export default InformationModal
+
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{
+              mt: 2,
+              py: 1.2,
+              backgroundColor: "primary.main",
+              color: "secondary.main",
+              "&:hover": {
+                backgroundColor: "primary.light",
+                color: "text.secondary",
+              },
+            }}
+            onClick={closeModal}
+          >
+            بستن
+          </Button>
+        </Box>
+      </Modal>
+    </>
+  );
+}
